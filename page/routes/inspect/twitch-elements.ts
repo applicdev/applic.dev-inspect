679662d70a0d55cfc4496b4ded3f050cbf4ae40e
Milo <54866561+MiloTheirself@users.noreply.github.com>
2022-07-27 22:42:02 +0200

    fix: relative import paths

--- page/routes/inspect/twitch-elements.ts
+++ page/routes/inspect/twitch-elements.ts
@@ -1,7 +1,11 @@
 import { resolve } from 'https://deno.land/std@0.149.0/path/mod.ts';
 
-const STATIC_INSPECT_PATH = resolve(import.meta.url.replace('file:///', ''), `../../../static/inspect/twitch-elements/index.html`);
-const STATIC_INSPECT = await Deno.readTextFile(STATIC_INSPECT_PATH);
+const STATIC_INSPECT_PATH = resolve(new URL('.', import.meta.url).pathname, `../../../static/inspect/twitch-elements/index.html`);
+const STATIC_INSPECT = await Deno.readTextFile(STATIC_INSPECT_PATH).catch((err: Error) => {
+  console.log('import.meta.url', new URL('.', import.meta.url).pathname);
+  console.log('STATIC_INSPECT_PATH', STATIC_INSPECT_PATH);
+  return err.toString();
+});
 
 export const handler = () => {
   return new Response(STATIC_INSPECT, {
